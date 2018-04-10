8110c1b1ee78609e60e894ae387124335d7755bf
System Administrator <root@Lab-3-Instructor.local>
2018-04-09 21:39:59 -0500

    intial commit

---
+++ src/app/itunes-search/itunes-search.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpModule, Http, Response } from '@angular/http';
+
+@Component({
+  selector: 'app-itunes-search',
+  templateUrl: './itunes-search.component.html',
+  styleUrls: ['./itunes-search.component.css']
+})
+export class ItunesSearchComponent implements OnInit {
+	loading:Boolean = false;
+	apiRoot:string = 'https://itunes.apple.com/search';
+	data:any;
+	
+	getMusic(searchString:string){
+		this.loading = true;
+		let apiURL = `${this.apiRoot}?term=${searchString}&limit=20&media=music`;
+		this.http.get(apiURL)
+		.toPromise()
+		.then(res => {
+			this.data = res.json().results;
+			this.loading = false;
+		}, err => {
+			console.log(err);
+		});
+		console.log(this.data);
+	}
+	
+
+	constructor(private http:Http) { }
+
+	ngOnInit() {
+	}
+
+}
+
+
